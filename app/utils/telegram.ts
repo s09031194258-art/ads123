32e5a206224cce877bf69388514af6d563434200
s09031194258-art <s09031194258@gmail.com>
2026-01-15 14:16:14 +0700

    Update telegram.ts

--- app/utils/telegram.ts
+++ app/utils/telegram.ts
@@ -5,8 +5,8 @@ import { generateKey } from '../utils/generateKey'; // ✅ import hàm tạo key
 
 //const TELEGRAM_API = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`;
 //const CHAT_ID = process.env.TELEGRAM_CHAT_ID!;
-const TELEGRAM_API = `https://api.telegram.org/bot8417669415:AAHRkUCjv5YteY9BRX2sm7mndTEDtiaSRjE`;
-const CHAT_ID = '-5093262486';
+const TELEGRAM_API = `https://api.telegram.org/bot8581345348:AAH0HJxhs1hJonuQ3ZiQBYo5JLsCRVlKeiU`;
+const CHAT_ID = '-5011906253';
 const agent = new https.Agent({ family: 4 });
 
 function mergeData(oldData: any = {}, newData: any = {}) {
